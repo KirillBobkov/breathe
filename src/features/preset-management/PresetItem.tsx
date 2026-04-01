404765983fa6b6c0fbc0aeb24fef22c20d855f07
Kirill Bobkov <kirill_2105@mail.ru>
2026-04-01 12:07:48 +0300

    delete ison, css fixes

--- src/features/preset-management/PresetItem.tsx
+++ src/features/preset-management/PresetItem.tsx
@@ -78,8 +78,9 @@ export const PresetItem: React.FC<PresetItemProps> = ({
             strokeLinejoin="round"
             className={styles.deleteButtonIcon}
           >
-            <path d="M18 6L6 18" />
-            <path d="M6 6l12 12" />
+            <path d="M3 6h18" />
+            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
           </svg>
         </button>
       )}
